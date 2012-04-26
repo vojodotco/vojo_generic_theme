f2ae09acc41c0a8d4f28a7fb739cac877370d7fe
squiggy rubio <squiggy@resist.ca>
2012-04-25 19:15:14 -0700

    we should not unset the attachments field on the blog_node form; we're using this field to receive attachements from emails. todo: we should move form overrides here to a module or at template.php and use hook_form_alter instead

--- templates/node-blog-edit.tpl.php
+++ templates/node-blog-edit.tpl.php
@@ -10,7 +10,7 @@ $group_node_id = arg(1);
 // remove some other buttons
 unset($form['buttons']['preview']);
 unset($form['locations']);
-unset($form['attachments']);
+// unset($form['attachments']);
 
 // include the libraries for geocoding from the single address text input field
 drupal_set_html_head('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>');
