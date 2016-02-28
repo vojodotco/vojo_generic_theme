2db3ad6b22114b2d9efda49114cdcbe6fa7bdfab
root <root@li327-191.members.linode.com>
2016-02-28 01:00:25 +0000

    https for doctypes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -112,8 +112,8 @@
 //print "in group ".$vojo_group_nid;        
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head>
     <title><?php print $head_title; ?></title>
